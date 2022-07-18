35f5a5bd2ce3fbc9a9feafbc6174b2b0b04de54d
ryan <ryan@aleph-labs.com>
2022-07-18 14:24:25 +0800

    pushh

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 import Table from './Table';
 import SearchInput from './SearchInput';
-import Filter from './Filter';
+import Filter from './Filter/index';
 import PaginationComponent from './Pagination';
 
 export { Table, SearchInput, Filter, PaginationComponent };
